b1210ba315e963a1075e9bd77e7e8b4e6fabd7b8
Sara Danielli <sara.dani94@gmail.com>
2023-10-25 10:28:37 -0400

    New script to save file

---
+++ scripts/download-fastq.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+# Define directories and filenames
+FASTQ_R1=subset-SRR11518889_1.fastq.gz
+FASTQ_URL=https://raw.githubusercontent.com/AlexsLemonade/reproducible-research/main/instructor_notes/fastq_subset
+
+#study ID
+STUDY_ID="SRP255885"
+
+#directory to save fastq
+OUTDIR=../data/raw/fastq/${STUDY_ID}
+#create OUTDIR
+mkdir -p ${OUTDIR}
+
+# Get files
+echo "Retrieving file ${FASTQ_R1}"
+curl -O ${FASTQ_URL}/${FASTQ_R1}
+echo "How many lines?"
+gunzip -c ${FASTQ_R1} | wc -l
+
+#Move file to final destination
+mv ${FASTQ_R1} ${OUTDIR}
